2bb1d50d8b48739d565ca1784e631f39101b37c7
kaufon <kauanfvale@gmail.com>
2025-08-26 10:45:59 -0300

    delete server

--- server/src/infra/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HttpModule } from 'src/infra/http/http.module';
-
-@Module({
-  imports: [HttpModule],
-})
-export class AppModule {}
--- server/src/infra/http/http.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { HiController } from "src/infra/http/controllers/hi.controller";
-
-@Module({
-	controllers: [HiController],
-})
-export class HttpModule {}
